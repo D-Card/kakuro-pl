a76855a659287d18686e45758f91319ee63df21d
D-Card <diogocardoso67@tecnico.ulisboa.pt>
2021-05-06 23:31:36 +0100

    Add predicates 8, 9, 10 and 11

--- kakuro.pl
+++ kakuro.pl
@@ -82,9 +82,56 @@ any(Cond, [P|R]) :-
 permutacoes_soma_espacos(Espacos, Perms_soma) :-
     bagof(X, aux_permutacoes_soma_espacos(Espacos, X), Perms_soma).
 
-aux_permutacoes_soma_espacos(Espacos, Perm_soma) :-
+aux_permutacoes_soma_espacos(Espacos, [Espaco, Perms]) :-
     member(Espaco, Espacos),
     Espaco = espaco(Soma, Vars),
     length(Vars, Length),
-    permutacoes_soma(Length, [1,2,3,4,5,6,7,8,9], Soma, Perms),
-    Perm_soma = [Espaco, Perms].
+    permutacoes_soma(Length, [1,2,3,4,5,6,7,8,9], Soma, Perms).
+
+
+permutacao_possivel_espaco(Perm ,Esp, Espacos, Perms_soma) :-
+    espacos_com_posicoes_comuns(Espacos, Esp, Esps_com),
+    member(Perm_soma, Perms_soma),
+    Perm_soma = [Esp, Perms_esp],
+    member(Perm, Perms_esp),
+    bagof(Pos, Var^(member(Var, Perm), nth0(Pos, Perm, Var)), Indices),
+    aux_permutacao_possivel_espaco(Esps_com, Perms_soma, Indices, Perm).
+
+
+aux_permutacao_possivel_espaco(_, _, [], _).
+aux_permutacao_possivel_espaco(Esps_com, Perms_soma, [P|R], Perm) :-
+    nth0(P, Perm, Var), 
+    nth0(P, Esps_com, Esp_com),           
+    member(Perm_soma, Perms_soma),
+    Perm_soma = [Esp_com, Perms_esp_com],
+    flatten(Perms_esp_com, Var_com),
+    member(Var, Var_com),
+    aux_permutacao_possivel_espaco(Esps_com, Perms_soma, R, Perm), !.
+
+permutacoes_possiveis_espaco(Espacos, Perms_soma, Esp, Perms_poss) :-
+    bagof(Perm, permutacao_possivel_espaco(Perm, Esp, Espacos, Perms_soma), Perms),
+    Esp = espaco(_, Vars),
+    Perms_poss = [Vars, Perms].
+
+permutacoes_possiveis_espacos(Espacos, Perms_poss_esps) :-
+    permutacoes_soma_espacos(Espacos, Perms_soma),
+    bagof(Perm, Espaco^(member(Espaco, Espacos), 
+    permutacoes_possiveis_espaco(Espacos, Perms_soma, Espaco, Perm)), Perms_poss_esps).
+
+
+numeros_comuns(Lst_perms, Numeros_comuns) :-
+    findall(Num_com, numero_comum(Lst_perms, Num_com), Numeros_comuns).
+    
+
+numero_comum(Lst_perms, Numero_comum) :-
+    mat_transposta(Lst_perms, Lst_perms_ind),
+    member(Perm_ind, Lst_perms_ind),
+    maplist(=(_), Perm_ind),
+    nth1(Pos, Lst_perms_ind, Perm_ind),
+    nth1(1, Perm_ind, Value),
+    Numero_comum = (Pos, Value).
+
+
+
+    
+    
